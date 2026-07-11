4710d6af8e8a3ea1a8f9cead527686427522b3d0
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-07-11 10:56:29 +0530

    variables and datatypes in python

--- 01_basics/variablesAndDatatypes.py
+++ 01_basics/variablesAndDatatypes.py
@@ -2,9 +2,16 @@
 name="kajal"
 age=23
 price=25.99
+old=False
+a=None
 
 print(name)
 print("My name is :",name)
 print("My age is :",age)
-print("Price is :",price)
 
+# Datatypes define the type of the variables
+print(type(name)) #String
+print(type(age)) #Integer
+print(type(price)) #Float
+print(type(old)) #boolean
+print(type(a)) #None
