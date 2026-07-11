1617231bc971f969081f2920179d7b5ec89ec947
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-07-11 09:45:22 +0530

    Initial commit

---
+++ 01_basics/variablesAndDatatypes.py
@@ -0,0 +1,10 @@
+# Avariable is a name given to a memory location in a program. 
+name="kajal"
+age=23
+price=25.99
+
+print(name)
+print("My name is :",name)
+print("My age is :",age)
+print("Price is :",price)
+
