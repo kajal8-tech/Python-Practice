65d929d6fa8a24159287b4cab4b28b03c3499004
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-07-26 08:40:05 +0530

    variables and rules for defining variables

---
+++ chapter-2/02_datatypes.py
@@ -0,0 +1,20 @@
+a = 1 # a is an integer
+
+b =2.23 # b is a floating point number
+
+c = "Kajal" # c is a string
+
+d = False # d is a bollean variable
+
+e = None # e is a none type variable
+
+# Rules for defining variables 
+
+aa = 456
+
+kajal = 45
+
+_Guddi = 39
+
+# @Sameer = 56 #invalid due to @ symbol
+# s@meer #invalid due to @ symbol
