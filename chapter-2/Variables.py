db52663a08bbda417445f8854b9c8ae119de6a50
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-07-26 08:29:07 +0530

    variables in python

---
+++ chapter-2/Variables.py
@@ -0,0 +1,10 @@
+# Varibles are cointaner to store a value
+a = 1
+b = 2
+c = 3
+
+name = "kajal"
+
+print(a+b)
+# Keywords are reserved words in python.
+# Identifiers class/function  or variable name.
