a9f31362ba2c79c39ebc16b88f980318b2438e70
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-07-26 09:30:17 +0530

    operators in python

---
+++ chapter-2/03_operators.py
@@ -0,0 +1,36 @@
+# Arithmetic Operators
+a = 34
+b = 4
+c = a+b
+print(c)
+
+#Assignment Operators
+d = 4-2 # Assign 4-2 in a
+print(d)
+
+e = 6 
+e += 3 # Increment the value of 'e' by 3 then assign it to 'e'
+e -= 3 # Decrement the value of 'e' by 3 then assign it to 'e'
+e *= 3 # Multiple the value of 'e' by 3 then assign it to 'e'
+e /= 3 # Divide the value of 'e' by 3 then assign it to 'e'
+print(e)
+
+# Comparison Operators
+
+f = 5<4
+print(f)
+
+# Logical Operators
+
+g = True or False
+# Truth table for 'OR' 
+print("True or true is true")
+print("True or false is true")
+print("False or true is true")
+print("False or False is true")
+
+# Truth table for 'AND'
+print("True and true is true")
+print("True and False is False")
+print("False and True is False")
+print("False and False is False")
