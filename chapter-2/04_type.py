4b4480af0deab330c2cd009667a4941b19499b0c
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-07-26 19:32:47 +0530

    Datatypes and Type conversion in python

---
+++ chapter-2/04_type.py
@@ -0,0 +1,9 @@
+# type() is used to find the datatype of a given variable in python
+
+a = "32.5"
+
+b = float(a) # String to float conversion
+
+t = type(b) # find the datatype of the variable 'b'
+
+print(t)
