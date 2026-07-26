d8e4569c2be7edd6a340547aa1eb1f62d84d5aef
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-07-26 19:42:26 +0530

    input in python

---
+++ chapter-2/05_input.py
@@ -0,0 +1,12 @@
+# input() allows the user to take input from the keyboard ass string.
+
+a = input("Enter 1st number: ")
+b = input("Enter 2nd number: ")
+
+print(a+b)
+
+# It takes the input from the user as integer
+c = int(input("Enter 1st number: "))
+d = int(input("Enter 2nd number: "))
+
+print(c+d)
