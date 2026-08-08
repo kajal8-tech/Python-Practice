df2ed4cd4662f974d88f5e052826490f4839b053
Kajalrani Sahu <kajalranisahu8@gmail.com>
2026-08-08 16:27:53 +0530

    String in Python

---
+++ chapter-3/01_introToString.py
@@ -0,0 +1,11 @@
+a = 'kajal' #Single Quoted string
+b = "kajal" #Double Quoted string
+c = '''kajal''' #Triple Quoted string
+length = len(a)
+shortname = a[2:5]
+
+print(a)
+print(b)
+print(c)
+print(length)
+print(shortname)
